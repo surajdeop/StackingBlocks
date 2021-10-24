76f1ba530ebb8c9ea7793ac239c7a3cc94414d9a
surajdeop <surajdeop@gmail.com>
2021-10-24 12:47:11 +0530

    Update StackHeightTest.java
    Added some test cases

--- src/test/java/com/anz/StackHeightTest.java
+++ src/test/java/com/anz/StackHeightTest.java
@@ -23,4 +23,29 @@ public class StackHeightTest {
         StackHeight stackHeight = new StackHeight(input);
         assertEquals(102, stackHeight.getMaxStackHeight());
     }
+
+    @Test
+    public void useCase4_all_cubes() {
+        String input = "[[7,7,7],[7,7,7],[7,7,7],[7,7,7],[7,7,7]]";
+        StackHeight stackHeight = new StackHeight(input);
+        assertEquals(35, stackHeight.getMaxStackHeight());
+    }
+
+    @Test
+    public void useCase5_mix_cubes_coboid() {
+        String input = "[[7,7,7],[38,25,45],[7,17,11],[7,7,7],[11,7,17],[76,35,3]]";
+        StackHeight stackHeight = new StackHeight(input);
+        assertEquals(93, stackHeight.getMaxStackHeight());
+    }
+
+    @Test
+    public void failedUseCase() {
+        String input = "[[7,11,17],[7,17,110],[11,7,17],[11,17,7],[17,7,11],[17,11,7]]";
+        try {
+            StackHeight stackHeight = new StackHeight(input);
+            stackHeight.getMaxStackHeight();
+        } catch (Exception e) {
+            assertEquals("Invalid Block Inputs, size of dimension must be 3 number separated by comma or dimension may not lie between 1 to 100", e.getMessage());
+        }
+    }
 }
